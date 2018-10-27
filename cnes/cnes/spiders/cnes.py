cbeea113d1483baf122595957576121f03c03d89
Anderson Berg <andersonberg@gmail.com>
2018-10-27 01:18:01 -0300

    fix state parse

--- cnes/cnes/spiders/cnes.py
+++ cnes/cnes/spiders/cnes.py
@@ -3,47 +3,53 @@
 import re
 import scrapy
 # import pandas as pd
+from urllib.parse import unquote, urljoin
 from cnes.items import CnesItem
 
 
 class CnesSpider(scrapy.Spider):
     name = 'cnes'
-    allowed_domains = ['cnes2.datasus.gov.br/']
+    allowed_domains = ['cnes2.datasus.gov.br']
     start_urls = ['http://cnes2.datasus.gov.br/Mod_Ind_Equipamento.asp']
+    start_urls = []
+    base_url = 'http://cnes2.datasus.gov.br/Mod_Ind_Equipamento.asp'
 
-    def parse(self, response):
-        for state in response.css(
-                "select[name='ComboEstado'] > option ::attr(value)").extract():
+    br_states = {
+        'ACRE': '12',
+        'ALAGOAS': '27',
+    }
+
+    def start_requests(self):
+        for state in self.br_states.values():
             if state == '00' or not state:
                 continue
-            state_url = f'http://cnes2.datasus.gov.br/Mod_Ind_Equipamento.asp?VEstado={state}'
+            state_url = unquote(urljoin(self.base_url, f'?VEstado={state}'))
             yield scrapy.Request(
                 state_url,
                 callback=self.parse_state,
                 meta={
                     'state_code': state,
                 },
-                dont_filter=True
             )
 
     def parse_state(self, response):
         state = response.meta['state_code']
-        for city in response.css(
-                "select[name='ComboMunicipio'] > option ::attr(value)").extract():
-            if not city:
-                continue
-            city_url = f'http://cnes2.datasus.gov.br/Mod_Ind_Equipamento.asp?VEstado={state}&VMun={city}'
-            yield scrapy.Request(
-                city_url,
-                callback=self.parse_city,
-                meta={
-                    'state_code': state,
-                    'city_code': city,
-                },
-            )
+        cities = response.css("select[name='ComboMunicipio'] > option ::attr(value)").extract()
+        for city in cities:
+            if city:
+                city_url = unquote(f'http://cnes2.datasus.gov.br/Mod_Ind_Equipamento.asp?VEstado={state}&VMun={city}')
+                yield scrapy.Request(
+                    city_url,
+                    callback=self.parse_city,
+                    meta={
+                        'state_code': state,
+                        'city_code': city,
+                    },
+                )
 
     def parse_city(self, response):
         table = response.xpath('//table[@border="1"]')
-        for sel in table.xpath('.//tr'):
-            line = sel.xpath('td/font/text()').extract()
-            print(line)
+        # for sel in table.xpath('.//tr'):
+        #     line = sel.xpath('td/font/text()').extract()
+        #     print(line)
+        return
