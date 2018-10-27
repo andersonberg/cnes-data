d1808382d088c6cb776e5375355697db7133b8e6
Anderson Berg <andersonberg@gmail.com>
2018-10-27 09:15:06 -0300

    fixing city parser

--- cnes/cnes/spiders/cnes.py
+++ cnes/cnes/spiders/cnes.py
@@ -10,7 +10,6 @@ from cnes.items import CnesItem
 class CnesSpider(scrapy.Spider):
     name = 'cnes'
     allowed_domains = ['cnes2.datasus.gov.br']
-    start_urls = ['http://cnes2.datasus.gov.br/Mod_Ind_Equipamento.asp']
     start_urls = []
     base_url = 'http://cnes2.datasus.gov.br/Mod_Ind_Equipamento.asp'
 
@@ -34,10 +33,12 @@ class CnesSpider(scrapy.Spider):
 
     def parse_state(self, response):
         state = response.meta['state_code']
-        cities = response.css("select[name='ComboMunicipio'] > option ::attr(value)").extract()
+        cities = response.css(
+            "select[name='ComboMunicipio'] > option ::attr(value)").extract()
         for city in cities:
             if city:
-                city_url = unquote(f'http://cnes2.datasus.gov.br/Mod_Ind_Equipamento.asp?VEstado={state}&VMun={city}')
+                city_url = unquote(
+                    urljoin(self.base_url, f'?VEstado={state}&VMun={city}'))
                 yield scrapy.Request(
                     city_url,
                     callback=self.parse_city,
@@ -48,8 +49,9 @@ class CnesSpider(scrapy.Spider):
                 )
 
     def parse_city(self, response):
+        city = response.meta['city_code']
         table = response.xpath('//table[@border="1"]')
-        # for sel in table.xpath('.//tr'):
-        #     line = sel.xpath('td/font/text()').extract()
-        #     print(line)
+        for sel in table.xpath('.//tr'):
+            line = sel.xpath('td/font/text()').extract()
+            equipamento = sel.xpath('td/font/a/text()').extract_first()
         return
